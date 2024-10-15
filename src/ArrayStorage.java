70fa0104ecdd98d530514c38dff6049f9a688d7e
Игорь_Подолян <tinyxxl@bk.ru>
2024-10-15 21:41:47 +0400

    Выполнил стартовое задание BaseJava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int resumeCount;
 
     void clear() {
+        if (storage[0] == null) return;
+        Arrays.fill(storage, 0, resumeCount, null);
+        resumeCount = 0;
     }
 
     void save(Resume r) {
+        if (resumeCount >= storage.length) return;
+        storage[resumeCount++] = r;
     }
 
     Resume get(String uuid) {
+        if (resumeCount == 0) return null;
+        for (int i = 0; i < resumeCount; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < resumeCount; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, resumeCount - i - 1);
+                storage[--resumeCount] = null;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[resumeCount];
+        System.arraycopy(storage, 0, resumes, 0, resumeCount);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return resumeCount;
     }
 }
