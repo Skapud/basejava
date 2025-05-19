30f8eb070d1bd7fc8be8e28fba3661fffc77f1fd
Игорь_Подолян <tinyxxl@bk.ru>
2025-05-19 22:05:33 +0400

    Tomcat - Servlet problem

---
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -0,0 +1,17 @@
+package com.urise.webapp.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Hello Resumes!");
+    }
+}
