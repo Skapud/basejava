53b25e9e653b8843ac3d1ee3272b06bb114bcc29
Игорь_Подолян <tinyxxl@bk.ru>
2025-06-03 16:57:51 +0400

    config fix

--- src/com/urise/webapp/web/ResumeServletHtml.java
+++ src/com/urise/webapp/web/ResumeServletHtml.java
@@ -1,11 +1,20 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.storage.Storage;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
 import java.io.IOException;
 
 public class ResumeServletHtml extends HttpServlet {
+    private Storage storage;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -18,5 +27,7 @@ public class ResumeServletHtml extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         String name = request.getParameter("name");
             response.getWriter().write(name == null ? "Hello resumes!" : "Hello " + name + " !");
+            request.setAttribute("resumes", storage.getAllSorted());
+            request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
